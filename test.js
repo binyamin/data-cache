1159329b5ad738b2d8e890be80b8a35bdcfa6597
Binyamin Green <binyamingreen@protonmail.com>
2026-01-12 06:33:30 -0800

    test: Move from ava to "node:test"

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-import test from 'ava';
-
-import mock from 'mock-fs'; // Import before 'node:fs'
+import assert from 'node:assert/strict';
 import fs from 'node:fs/promises';
+import { test } from 'node:test';
+
+import mock from 'mock-fs';
 
 import * as datacache from './index.js';
 
@@ -13,39 +14,39 @@ test.after(() => {
 	mock.restore();
 });
 
-test.serial('#set should set key if value is provided', async (t) => {
+test('#set should set key if value is provided', async () => {
 	datacache.set('username', 'binyamin');
 
 	const content = await fs.readFile('.cache/username', 'utf-8');
-	t.is(content, 'binyamin');
+	assert.equal(content, 'binyamin');
 });
 
-test('#set should throw if value is not present', (t) => {
-	t.throws(() => datacache.set('username'));
+test('#set should throw if value is not present', () => {
+	assert.throws(() => datacache.set('username'));
 });
 
-test('#get should return a value, if key exists', (t) => {
+test('#get should return a value, if key exists', () => {
 	const username = datacache.get('username');
-	t.is(username, 'binyamin');
+	assert.equal(username, 'binyamin');
 });
 
-test('#get should return `null`, if key does not exist', (t) => {
+test('#get should return `null`, if key does not exist', () => {
 	const email = datacache.get('email');
-	t.is(email, null);
+	assert.equal(email, null);
 });
 
-test('#get should return an object/record, if key points to folder', (t) => {
+test('#get should return an object/record, if key points to folder', () => {
 	datacache.set('person.username', 'binyamin');
 	const person = datacache.get('person');
-	t.deepEqual(person, {
+	assert.deepEqual(person, {
 		username: 'binyamin',
 	});
 });
 
-test('#get should return a value, if data is JSON and `ext` was not present', (t) => {
+test('#get should return a value, if data is JSON and `ext` was not present', () => {
 	datacache.set('person2', { username: 'binyamin' });
 	const person = datacache.get('person2');
-	t.deepEqual(person, {
+	assert.deepEqual(person, {
 		username: 'binyamin',
 	});
 });
